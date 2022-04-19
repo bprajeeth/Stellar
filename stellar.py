932317a29d1209a4855da0e29514d42997e532f1
bprajeeth <84741032+bprajeeth@users.noreply.github.com>
2022-04-19 21:18:31 +0530

    Update stellar.py

--- stellar.py
+++ stellar.py
@@ -7,28 +7,29 @@ white=(255,255,255)
 yellow=(255,255,0)
 green=(0,255,0)
 rblue=(65,105,225)
+grey=(211,211,211)
 
 def checkguess(window,value,word,attempt): #function to check if entered word is right and also to over write boxes with their respective colours
-    font = pygame.font.Font(None, 50) 
+    font = pygame.font.SysFont("arial", 35) 
     word_list=list(word)
     val=list(value)
     boxcolour=[white,white,white,white,white,white]
     for i in range(6):
         if(val[i] == word_list[i]):
             boxcolour[i]=green
-        if(val[i] != word_list[i] and val[i] in word_list):
+        elif(val[i] != word_list[i] and val[i] in word_list):
             boxcolour[i]=yellow
+        else:
+            boxcolour[i]=grey    
         if(val[i] in word_list):    
             position=word_list.index(val[i])    
-            word_list[position]=0
-            #print(position) 
-        #print(word_list)   
-    #print(boxcolour)         
-
+            word_list[position]=0       
+            
     for i in range(6):
         displetter=font.render(val[i],True,black)
         pygame.draw.rect(window,boxcolour[i],pygame.Rect(45+(i*10+i*60),50+(attempt*(15+60)),60,60))
         window.blit(displetter,((60+(i*10+i*60)),(65+(attempt*(15+60)))))
+
 
     if(boxcolour == [green,green,green,green,green,green]):
         return True  
@@ -38,10 +39,11 @@ def checkguess(window,value,word,attempt): #function to check if entered word is
 width=500
 height=600
 lossmessage="Better Luck Next Time!"
-winmessage="To Infinity and Beyond!"
+winmessage="TO INFINITY AND BEYOND!"
 f=open("stellarwords.txt","r")
 wordlist=f.readlines()
 word=wordlist[random.randint(0,len(wordlist)-1)].upper()
+#print(wordlist)
 print("word = ",word)
 window = pygame.display.set_mode((width,height))
 pygame.display.set_caption('Stellar') 
@@ -52,8 +54,8 @@ val=""
 wordpresent=1
 FPS=30
 clock=pygame.time.Clock()
-font = pygame.font.Font(None, 40) 
-font2 = pygame.font.SysFont("Comic Sans",30)
+font = pygame.font.SysFont("arial", 35) 
+font2 = pygame.font.SysFont("arial",30)
 attempt=0
 title=font2.render("Stellar",True,(255,69,0))
 window.blit(title,(182,8))
@@ -61,6 +63,7 @@ for i in range(5):
     for j in range(6):
         pygame.draw.rect(window,white,pygame.Rect(45+(j*10+j*60),50+(i*(15+60)),60,60))
 while (run):
+
     for event in pygame.event.get():
         if(event.type == pygame.QUIT):
                 run=False
@@ -68,6 +71,8 @@ while (run):
             if(event.type == pygame.KEYDOWN):
                 x=event.unicode
                 val=val+ x.upper()
+                              
+        
                 if(event.key == pygame.K_BACKSPACE):
                     val = val[0:-2]
                     #print("val = ",val)
@@ -76,7 +81,7 @@ while (run):
                     val=val[:-2]+val[-1]   
 
                 if((event.key == pygame.K_RETURN) and len(val)>6):
-                    print(val)
+                    #print(val)
                     if(val[:6]+"\n" in wordlist):
                         wordpresent=1
                         win=checkguess(window,val,word,attempt)    
@@ -88,21 +93,22 @@ while (run):
                         #window.blit(dispenteragain,(100,500))
                         #print("please enter valid word") 
                         wordpresent=0
-                        val=""     
+                        val=""   
+                    
 
     window.fill(rblue,(0,430,500,200))   
     x=170    
     if(wordpresent == 0):
         window.blit(dispenteragain,(10,450))    
-        x=250
+        x=300
     if(win == True):
         dispwin=font.render(winmessage,True,white)
-        window.blit(dispwin,(95,500))    
+        window.blit(dispwin,(30,500))    
     if(attempt == 5 and win == False):
         disploss=font.render(lossmessage,True,white)
         actual_word=font.render("The Word is "+word[:-1],True,white)
-        window.blit(disploss,(100,500))         
-        window.blit(actual_word,(100,540))
+        window.blit(disploss,(80,500))         
+        window.blit(actual_word,(87,540))
     guess=font.render(val,True,white)
     window.blit(guess,(x,450))             
     pygame.display.update()
